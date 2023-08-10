37542beedbca35303ced0a9a9f3b11c7320e499a
ziming <ziming@users.noreply.github.com>
2023-08-10 13:36:23 +0800

    Remove loaded on request v3 optimization to see if it fix display of colours

--- src/FilamentStaticChartWidgetsServiceProvider.php
+++ src/FilamentStaticChartWidgetsServiceProvider.php
@@ -27,7 +27,7 @@ class FilamentStaticChartWidgetsServiceProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
         FilamentAsset::register([
-            Css::make(static::$name, __DIR__. '/../resources/dist/filament-static-chart-widgets.css')->loadedOnRequest(),
+            Css::make(static::$name, __DIR__. '/../resources/dist/filament-static-chart-widgets.css'),
         ], 'jonquihote/' . static::$name);
     }
 }
